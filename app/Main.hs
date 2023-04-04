842c6927f6afd752777d0ca7df733a23776ff945
l-Shane-l <shane.hamilton3@mail.dcu.ie>
2023-04-04 02:17:58 +0100

    attempt at html approach

--- app/Main.hs
+++ app/Main.hs
@@ -6,8 +6,7 @@ import Web.Scotty
 
 main :: IO ()
 main = scotty 3000 $ do
-  get "/" $ do
-    html "<h1>Welcome to Our Wedding</h1><p>Please enter your guest code to proceed:</p><form method='post' action='/login'><input type='password' name='password'><input type='submit' value='Login'></form>"
+  get "/" $ file "static/index.html"
 
   post "/login" $ do
     password <- param "password" :: ActionM Text
@@ -15,14 +14,26 @@ main = scotty 3000 $ do
       then do
         setHeader "Set-Cookie" "password=guest123; Path=/; HttpOnly"
         redirect "/main"
-      else text "Incorrect password."
+      else redirect "/failed-login"
 
-  get "/main" $ do
-    maybeCookie <- header "Cookie"
-    case maybeCookie of
-      Just cookie ->
-        if "password=guest123" `elem` words (unpack cookie) -- replace with actual password list or check
-          then html "<h1>Wedding Information</h1><p>Welcome to our wedding! Here's all the information you need.</p>"
-          else status unauthorized401 >> text "Unauthorized access."
-      Nothing ->
-        status unauthorized401 >> text "Unauthorized access."
+  get "/failed-login" $ file "static/failed-login.html"
+
+  get "/main" $ checkAuth $ file "static/main.html"
+  get "/programme" $ checkAuth $ file "static/programme.html"
+  get "/venue" $ checkAuth $ file "static/venue.html"
+  get "/accommodation" $ checkAuth $ file "static/accommodation.html"
+  get "/gifts" $ checkAuth $ file "static/gifts.html"
+  get "/photos" $ checkAuth $ file "static/photos.html"
+  get "/guestbook" $ checkAuth $ file "static/guestbook.html"
+  get "/transportation" $ checkAuth $ file "static/transportation.html"
+
+checkAuth :: ActionM () -> ActionM ()
+checkAuth action = do
+  maybeCookie <- header "Cookie"
+  case maybeCookie of
+    Just cookie ->
+      if "password=guest123" `elem` words (unpack cookie) -- replace with actual password list or check
+        then action
+        else status unauthorized401 >> text "Unauthorized access."
+    Nothing ->
+      status unauthorized401 >> text "Unauthorized access."
