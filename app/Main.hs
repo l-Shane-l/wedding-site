5842773955267d3c8d3635138de20e61b673a5fc
l-Shane-l <shane.hamilton3@mail.dcu.ie>
2023-04-04 03:10:49 +0100

    updates to main.hs

--- app/Main.hs
+++ app/Main.hs
@@ -2,22 +2,22 @@
 
 module Main where
 
+import AccomodationPage (accomodationPage)
+import GiftsPage (giftsPage)
+import GuestBookPage (guestBookPage)
+import Layout (baseTemplate)
+import LoginPage (failedLoginPage, loginPage)
 import qualified Lucid as L
+import MainPage (mainPage)
 import Network.HTTP.Types.Status (unauthorized401)
-import qualified Templates.AccommodationPage as AccommodationPage
-import qualified Templates.GiftsPage as GiftsPage
-import qualified Templates.GuestBookPage as GuestBookPage
-import qualified Templates.Layout as Layout
-import qualified Templates.LoginPage as LoginPage
-import qualified Templates.MainPage as MainPage
-import qualified Templates.PhotosVideoPage as PhotosVideoPage
-import qualified Templates.TransportationPage as TransportationPage
-import qualified Templates.VenuePage as VenuePage
+import PhotosVideoPage (photosVideoPage)
+import TransportationPage (transportationPage)
+import VenuePage (venuePage)
 import Web.Scotty
 
 main :: IO ()
 main = scotty 3000 $ do
-  get "/" $ html $ L.renderText $ Layout.baseTemplate "Our Wedding" LoginPage.loginPage
+  get "/" $ html $ L.renderText $ baseTemplate "Our Wedding" loginPage
 
   post "/login" $ do
     password <- param "password" :: ActionM L.Text
@@ -27,16 +27,16 @@ main = scotty 3000 $ do
         redirect "/main"
       else redirect "/failed-login"
 
-  get "/failed-login" $ html $ L.renderText $ Layout.baseTemplate "Login Failed" LoginPage.failedLoginPage
+  get "/failed-login" $ html $ L.renderText $ baseTemplate "Login Failed" failedLoginPage
 
-  get "/main" $ checkAuth $ html $ L.renderText $ Layout.baseTemplate "Wedding Information" MainPage.mainPage
-  get "/programme" $ checkAuth $ html $ L.renderText $ Layout.baseTemplate "Programme" MainPage.mainPage
-  get "/venue" $ checkAuth $ html $ L.renderText $ Layout.baseTemplate "Venue Information" VenuePage.venuePage
-  get "/accommodation" $ checkAuth $ html $ L.renderText $ Layout.baseTemplate "Accommodation Information" AccommodationPage.accommodationPage
-  get "/gifts" $ checkAuth $ html $ L.renderText $ Layout.baseTemplate "Gifts" GiftsPage.giftsPage
-  get "/photos" $ checkAuth $ html $ L.renderText $ Layout.baseTemplate "Photo and Video Upload" PhotosVideoPage.photosVideoPage
-  get "/guestbook" $ checkAuth $ html $ L.renderText $ Layout.baseTemplate "Guest Book" GuestBookPage.guestBookPage
-  get "/transportation" $ checkAuth $ html $ L.renderText $ Layout.baseTemplate "Transportation" TransportationPage.transportationPage
+  get "/main" $ checkAuth $ html $ L.renderText $ baseTemplate "Wedding Information" mainPage
+  get "/programme" $ checkAuth $ html $ L.renderText $ baseTemplate "Programme" mainPage
+  get "/venue" $ checkAuth $ html $ L.renderText $ baseTemplate "Venue Information" venuePage
+  get "/accommodation" $ checkAuth $ html $ L.renderText $ baseTemplate "Accommodation Information" accomodationPage
+  get "/gifts" $ checkAuth $ html $ L.renderText $ baseTemplate "Gifts" giftsPage
+  get "/photos" $ checkAuth $ html $ L.renderText $ baseTemplate "Photo and Video Upload" photosVideoPage
+  get "/guestbook" $ checkAuth $ html $ L.renderText $ baseTemplate "Guest Book" guestBookPage
+  get "/transportation" $ checkAuth $ html $ L.renderText $ baseTemplate "Transportation" transportationPage
 
 checkAuth :: ActionM () -> ActionM ()
 checkAuth action = do
